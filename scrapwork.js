839aa7906f0c58990b53fa6cff3bc27a64ba4df7
jessethatcher <jessethatcher721@icloud.com>
2024-06-06 13:14:16 -0500

    added push code

--- scrapwork.js
+++ scrapwork.js
@@ -1,1 +1,14 @@
 "hello world"
+/* 
+
+…or create a new repository on the command line
+echo "# Scrap-boi" >> README.md
+git init
+git add README.md
+git commit -m "Added readme to assert synchrological order"
+git branch -M main
+git remote add origin https://github.com/Kurisone/Scrap-boi.git
+git push -u origin main
+*/
+
+"Hello World"
