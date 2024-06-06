685b9201ec60a40e953e49cd381cab705a001d2d
jessethatcher <jessethatcher721@icloud.com>
2024-06-06 13:48:17 -0500

    console.log even and odds

--- TestPush.js
+++ TestPush.js
@@ -8,3 +8,11 @@ return i
 }
 
 console.log(stringStr(7))
+
+console.log(stringStr(2))
+console.log(stringStr(4))
+console.log(stringStr(6))
+console.log(stringStr(1))
+console.log(stringStr(3))
+console.log(stringStr(5))
+console.log(stringStr(8))
