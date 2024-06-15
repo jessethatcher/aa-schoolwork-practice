8fb8610d6351d21ccfea09379c6100ab00ce1079
jessethatcher <jessethatcher721@icloud.com>
2024-06-15 18:52:43 -0400

    Lost these

---
+++ ScrapOther/TestPush.js
@@ -0,0 +1,18 @@
+let i = "Bababooey"
+
+function stringStr(num){
+if(num % 2 === 0){
+  return 'Gooning'
+} else
+return i
+}
+
+console.log(stringStr(7))
+
+console.log(stringStr(2))
+console.log(stringStr(4))
+console.log(stringStr(6))
+console.log(stringStr(1))
+console.log(stringStr(3))
+console.log(stringStr(5))
+console.log(stringStr(8))
